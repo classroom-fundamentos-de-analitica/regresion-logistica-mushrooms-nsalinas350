2a3934691c8c050e33b44086a85a0edc3ce26bcf
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-23 13:09:24 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -57,27 +57,31 @@ La información contenida en la muestra es la siguiente:
 """
 
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
 
 def pregunta_01():
     """
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv('mushrooms.csv')
 
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
-    ____.____(____)
+    df = df.drop('veil-type', axis=1)
 
     # Asigne la columna `type` a la variable `y`.
-    ____ = ____
+    y = df['type']
 
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)
+    X = df.copy()
 
     # Remueva la columna `type` del DataFrame `X`.
-    ____.____(____)
+    X = X.drop('type', axis=1)
 
     # Retorne `X` y `y`
     return X, y
@@ -88,19 +92,13 @@ def pregunta_02():
     Preparación del dataset.
     """
 
-    # Importe train_test_split
-    from ____ import ____
-
     # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
     X, y = pregunta_01()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 123. Use 50 patrones para la muestra de prueba.
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=123
     )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
@@ -119,11 +117,6 @@ def pregunta_03():
     pipeline.
     """
 
-    # Importe LogisticRegressionCV
-    # Importe OneHotEncoder
-    # Importe Pipeline
-    from ____ import ____
-
     # Cargue las variables.
     X_train, _, y_train, _ = pregunta_02()
 
@@ -131,13 +124,13 @@ def pregunta_03():
     # LogisticRegression con una regularización Cs=10
     pipeline = Pipeline(
         steps=[
-            ("____", ____()),
-            ("____", ____(____)),
+            ("encoder", OneHotEncoder()),
+            ("classifier", LogisticRegressionCV(Cs=10)),
         ],
     )
 
     # Entrene el pipeline con los datos de entrenamiento.
-    ____.____(____, ____)
+    pipeline.fit(X_train, y_train)
 
     # Retorne el pipeline entrenado
     return pipeline
@@ -148,9 +141,6 @@ def pregunta_04():
     Evalue el modelo obtenido.
     """
 
-    # Importe confusion_matrix
-    from ____ import ____
-
     # Obtenga el pipeline de la pregunta 3.
     pipeline = pregunta_03()
 
@@ -158,14 +148,14 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_train = confusion_matrix(
+        y_true=y_train,
+        y_pred=pipeline.predict(X_train),
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = confusion_matrix(
+        y_true=y_test,
+        y_pred=pipeline.predict(X_test),
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
